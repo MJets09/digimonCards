285223cc2001f9d9676f60c26faf9da55cba93b0
MJets09 <mthaoqk@gmail.com>
2022-05-09 22:23:41 -0500

    Tried to add search by card set but was unable, need to look into

--- assests/JS/js.js
+++ assests/JS/js.js
@@ -3,13 +3,41 @@ document.querySelector('#getMon').addEventListener('click', getMon)
 document.querySelector('#dataType').onchange = function() {
     return document.querySelector('#dataType').value = this.value
 }
-
+document.querySelector('#cardSet').onchange = function() {
+    return document.querySelector('#cardSet').value = this.value
+}
 
 //This function removes the created elements.FIGURE OUT HOW THIS WORKS.
 function removeElements(elements) {
     for (var i = 0; i < elements.length; i++) {
         elements[i].parentNode.removeChild(elements[i]);
     }
+}
+
+
+function getMon() {
+    let userMon = document.querySelector('input').value
+    let userAttribute = document.querySelector('#dataType').value
+    let set = document.querySelector('#cardSet').value
+    let url = `https://digimoncard.io/api-public/search.php?n=${userMon}&color=${userAttribute}&set_name=${set}`
+
+    fetch(url).then(res => res.json()).then(data => {
+            console.log(data)
+
+            removeElements(document.querySelectorAll('li'));
+            //This removes the created elements.FIGURE OUT HOW THIS WORKS.
+            console.log(set)
+            data.forEach(obj => {
+                let newLi = document.createElement('li');
+                let newImg = document.createElement('img');
+                newImg.src = obj.image_url;
+                newLi.appendChild(newImg)
+                document.querySelector('ul').appendChild(newLi)
+            })
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
 }
 
 function getAllMon() {
@@ -30,35 +58,6 @@ function getAllMon() {
 
                 newLi.appendChild(newImg)
 
-                document.querySelector('ul').appendChild(newLi)
-            })
-
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        })
-
-}
-
-
-
-
-
-function getMon() {
-    let userMon = document.querySelector('input').value
-    let userAttribute = document.querySelector('select').value
-    let url = `https://digimoncard.io/api-public/search.php?n=${userMon}&color=${userAttribute}`
-
-    fetch(url).then(res => res.json()).then(data => {
-            console.log(data)
-
-            removeElements(document.querySelectorAll('li'));
-            //This removes the created elements.FIGURE OUT HOW THIS WORKS.
-            data.forEach(obj => {
-                let newLi = document.createElement('li');
-                let newImg = document.createElement('img');
-                newImg.src = obj.image_url;
-                newLi.appendChild(newImg)
                 document.querySelector('ul').appendChild(newLi)
             })
 
