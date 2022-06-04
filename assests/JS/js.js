d8e62af330b175be1aa4801bb691e0de6ffa0fd4
MJets09 <mthaoqk@gmail.com>
2022-06-04 12:43:28 -0500

    Cleaned up code, need to figure out how to search without any querys cept for name

--- assests/JS/js.js
+++ assests/JS/js.js
@@ -1,10 +1,6 @@
-document.querySelector('#getAllMon').addEventListener('click', getAllMon)
 document.querySelector('#getMon').addEventListener('click', getMon)
 document.querySelector('#dataType').onchange = function() {
     return document.querySelector('#dataType').value = this.value
-}
-document.querySelector('#cardSet').onchange = function() {
-    return document.querySelector('#cardSet').value = this.value
 }
 
 //This function removes the created elements.FIGURE OUT HOW THIS WORKS.
@@ -14,19 +10,17 @@ function removeElements(elements) {
     }
 }
 
-
 function getMon() {
-    let userMon = document.querySelector('input').value
-    let userAttribute = document.querySelector('#dataType').value
-    let set = document.querySelector('#cardSet').value
-    let url = `https://digimoncard.io/api-public/search.php?n=${userMon}&color=${userAttribute}&set_name=${set}`
+    let userMon = document.querySelector('#monName').value.toLowerCase();
+    let userColor = document.querySelector('#dataType').value;
+    let url = `https://digimoncard.io/api-public/search.php?n=${userMon}&color=${userColor}`;
+
 
     fetch(url).then(res => res.json()).then(data => {
             console.log(data)
 
             removeElements(document.querySelectorAll('li'));
             //This removes the created elements.FIGURE OUT HOW THIS WORKS.
-            console.log(set)
             data.forEach(obj => {
                 let newLi = document.createElement('li');
                 let newImg = document.createElement('img');
@@ -38,32 +32,4 @@ function getMon() {
         .catch(err => {
             console.log(`error ${err}`)
         })
-}
-
-function getAllMon() {
-    let userMon = document.querySelector('input').value
-    let url = `https://digimoncard.io/api-public/search.php?n=${userMon}`
-
-
-    fetch(url).then(res => res.json()).then(data => {
-            console.log(data)
-            data.forEach(obj => {
-
-                removeElements(document.querySelectorAll('li'));
-
-                let newLi = document.createElement('li');
-                let newImg = document.createElement('img');
-
-                newImg.src = obj.image_url;
-
-                newLi.appendChild(newImg)
-
-                document.querySelector('ul').appendChild(newLi)
-            })
-
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        })
-
 }
